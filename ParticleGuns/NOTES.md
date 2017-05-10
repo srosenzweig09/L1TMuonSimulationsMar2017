1e131bd986a6a09cc5f44fbab9cfd571d2cfd573
Jia Fu Low <jia.fu.low@cern.ch>
2017-05-09 21:22:06 -0400

    Initial commit fixup

---
+++ ParticleGuns/NOTES.md
@@ -0,0 +1,1 @@
+These codes are adapted from the CMSSW_9_0_0 version of IOMC/ParticleGuns.
---
+++ ParticleGuns/src/SealModule.cc
@@ -0,0 +1,14 @@
+
+#include "FWCore/Framework/interface/MakerMacros.h"
+// #include "IOMC/Input/interface/MCFileSource.h"
+
+// Julia Yarba : related to particle gun prototypes
+//
+
+#include "L1TMuonSimulations/ParticleGuns/interface/FlatRandomPtGunProducer2.h"
+
+// particle gun prototypes
+//
+
+using edm::FlatRandomPtGunProducer2;
+DEFINE_FWK_MODULE(FlatRandomPtGunProducer2);
