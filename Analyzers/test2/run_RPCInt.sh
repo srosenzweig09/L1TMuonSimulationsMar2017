d4459cb309c5ada76f98c69667328fb825c0bbc8
Jia Fu Low <jia.fu.low@cern.ch>
2017-07-07 21:48:33 -0400

    Import run scripts from the old repository

---
+++ Analyzers/test2/run_RPCInt.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+PIP=RPCInt.py
+PIPPO=pippo_RPCInt.py
+
+
+echo "running simEmtfDigisCSC*simEmtfDigisRPC*simEmtfDigisGEM*simEmtfDigis"
+cp $PIP $PIPPO
+cat <<EOF >>$PIPPO
+# Run quadruple EMTF emulators
+process.simEmtfDigisCSC = process.simEmtfDigis.clone(RPCEnable = False, GEMEnable = False, IRPCEnable = False, TTEnable = False)
+process.simEmtfDigisRPC = process.simEmtfDigis.clone(RPCEnable = True , GEMEnable = False, IRPCEnable = False, TTEnable = False)
+process.simEmtfDigisGEM = process.simEmtfDigis.clone(RPCEnable = True , GEMEnable = True , IRPCEnable = False, TTEnable = False)
+process.step1.replace(process.simEmtfDigis, process.simEmtfDigisCSC*process.simEmtfDigisRPC*process.simEmtfDigisGEM*process.simEmtfDigis)
+process.RAWSIMoutput.outputCommands.append('keep *_simEmtfDigis*_*_*')
+
+process.RAWSIMoutput.fileName = cms.untracked.string('l1NtupleMC_RAW2DIGI.full.root')
+
+process.maxEvents.input = cms.untracked.int32(-1)
+
+process.MessageLogger.cerr.FwkReport.reportEvery = 1000
+EOF
+cmsRun $PIPPO >pippo_RPCInt.log 2>&1
+
---
+++ Analyzers/test2/run_rpcintegration_cfg.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+PIP=rpcintegration_cfg.py
+PIPPO=pippo_rpcintegration_cfg.py
+
+
+echo "running csc"
+cp $PIP $PIPPO
+echo "process.source.fileNames = cms.untracked.vstring('file:l1NtupleMC_RAW2DIGI.full.root')" >> $PIPPO
+echo "process.rpcintegration.verbosity = cms.untracked.int32(0)" >> $PIPPO
+echo "process.rpcintegration.emuHitTag   = cms.InputTag('simEmtfDigisCSC')" >> $PIPPO
+echo "process.rpcintegration.emuTrackTag = cms.InputTag('simEmtfDigisCSC')" >> $PIPPO
+echo "process.rpcintegration.outFileName = cms.string('histos.csc.root')"   >> $PIPPO
+cmsRun $PIPPO >/dev/null 2>&1
+
+echo "running rpc"
+cp $PIP $PIPPO
+echo "process.source.fileNames = cms.untracked.vstring('file:l1NtupleMC_RAW2DIGI.full.root')" >> $PIPPO
+echo "process.rpcintegration.verbosity = cms.untracked.int32(0)" >> $PIPPO
+echo "process.rpcintegration.emuHitTag   = cms.InputTag('simEmtfDigisRPC')" >> $PIPPO
+echo "process.rpcintegration.emuTrackTag = cms.InputTag('simEmtfDigisRPC')" >> $PIPPO
+echo "process.rpcintegration.outFileName = cms.string('histos.rpc.root')"   >> $PIPPO
+cmsRun $PIPPO >/dev/null 2>&1
+
+echo "running gem"
+cp $PIP $PIPPO
+echo "process.source.fileNames = cms.untracked.vstring('file:l1NtupleMC_RAW2DIGI.full.root')" >> $PIPPO
+echo "process.rpcintegration.verbosity = cms.untracked.int32(0)" >> $PIPPO
+echo "process.rpcintegration.emuHitTag   = cms.InputTag('simEmtfDigisGEM')"    >> $PIPPO
+echo "process.rpcintegration.emuTrackTag = cms.InputTag('simEmtfDigisGEM')"    >> $PIPPO
+echo "process.rpcintegration.outFileName = cms.string('histos.gem.root')"   >> $PIPPO
+cmsRun $PIPPO >/dev/null 2>&1
+
+echo "running irpc"
+cp $PIP $PIPPO
+echo "process.source.fileNames = cms.untracked.vstring('file:l1NtupleMC_RAW2DIGI.full.root')" >> $PIPPO
+echo "process.rpcintegration.verbosity = cms.untracked.int32(0)" >> $PIPPO
+echo "process.rpcintegration.emuHitTag   = cms.InputTag('simEmtfDigis')"    >> $PIPPO
+echo "process.rpcintegration.emuTrackTag = cms.InputTag('simEmtfDigis')"    >> $PIPPO
+echo "process.rpcintegration.outFileName = cms.string('histos.irpc.root')"   >> $PIPPO
+cmsRun $PIPPO >/dev/null 2>&1
+
