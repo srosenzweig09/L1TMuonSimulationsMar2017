f8a2bd81a200027747a11540e84b363fdce94bfc
Jia Fu Low <jia.fu.low@cern.ch>
2017-07-19 15:34:23 -0400

    Update tools.py

--- Configuration/python/tools.py
+++ Configuration/python/tools.py
@@ -1,9 +1,14 @@
 import os
 
-def loadFromFile(filename):
+def loadFromFile(filename, fmt=''):
+  """
+  'filename' is the filename of the text file which contains the list of all the filenames.
+  'fmt' is a formatting string to change the filenames. (default: '')
+  """
   if not os.path.exists(filename):
     raise RuntimeError('Bad filename: %s' % filename)
   lines = tuple(open(filename))
   lines = [line.strip() for line in lines if not line.lstrip().startswith('#')]  # remove comment lines
+  if fmt:  lines = [fmt % line for line in lines]
   return lines
 
