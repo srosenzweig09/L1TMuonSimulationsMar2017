5444007da36303f48a8437cc8b8aa77032748eec
Jia Fu Low <jia.fu.low@cern.ch>
2018-07-22 03:43:25 -0400

    Update nn scripts [2]

--- Analyzers/test8/nn_main.py
+++ Analyzers/test8/nn_main.py
@@ -8,5 +8,32 @@ from nn_models import create_model, create_model_sequential
 
 from nn_training import train_model, train_model_sequential
 
+# ______________________________________________________________________________
+# Import muon data
+x_train, x_test, y_train, y_test, w_train, w_test, x_mask_train, x_mask_test = \
+    muon_data_split(infile_muon, adjust_scale=adjust_scale, reg_pt_scale=reg_pt_scale, test_size=0.275)
+
+# Add discrimator output node
+if add_noise:
+  labels = np.where(np.abs(1.0/y_train) > discr_pt_cut, 1., 100.)  # mask_value is set to 100
+  y_train = [y_train, labels.astype(np.float32)]
+  labels = np.where(np.abs(1.0/y_test) > discr_pt_cut, 1., 100.)  # mask_value is set to 100
+  y_test = [y_test, labels.astype(np.float32)]
 
 # ______________________________________________________________________________
+# Import pileup data
+pu_x_train, pu_x_test, pu_aux_train, pu_aux_test, pu_w_train, pu_w_test, pu_x_mask_train, pu_x_mask_test = \
+    pileup_data_split(infile_pileup, adjust_scale=adjust_scale, reg_pt_scale=reg_pt_scale, discr_pt_cut=discr_pt_cut, test_job=30)
+
+# Add output nodes for pileup data
+if add_noise:
+  pu_y_train = [np.zeros((pu_x_train.shape[0],), dtype=np.float32) + 100., np.zeros((pu_x_train.shape[0],), dtype=np.float32)]  # mask_value is set to 100
+  pu_y_test = [np.zeros((pu_x_test.shape[0],), dtype=np.float32) + 100., np.zeros((pu_x_test.shape[0],), dtype=np.float32)]  # mask_value is set to 100
+
+# ______________________________________________________________________________
+# Create models
+
+model = create_model(nvariables=nvariables, lr=learning_rate, discr_loss_weight=discr_loss_weight)
+
+model_seq = create_model_sequential(nvariables=nvariables, lr=learning_rate)
+
