f0e0a461b2e02cf224afd652de83a14f6d2379e3
Jia Fu Low <jia.fu.low@cern.ch>
2018-08-01 15:21:00 -0400

    Update test8 [5]

--- Analyzers/test8/send_to_hpe.sh
+++ Analyzers/test8/send_to_hpe.sh
@@ -1,4 +1,6 @@
 #scp histos_tba.14.npz histos_tbd.14.npz jlow@15.226.54.11:/scratch/CMS/L1MuonTrigger/P2_10_1_5/SingleMuon_Toy_2GeV/
+#scp histos_tba.15.npz histos_tbd.15.npz jlow@15.226.54.11:/scratch/CMS/L1MuonTrigger/P2_10_1_5/SingleMuon_Toy_2GeV/
+#scp histos_tba.16.npz histos_tbd.16.npz jlow@15.226.54.11:/scratch/CMS/L1MuonTrigger/P2_10_1_5/SingleMuon_Toy_2GeV/
 
-tar czf hpe_default.tgz nn_*.py mykeras4.ipynb tdrstyle.mplstyle
+tar czf hpe_default.tgz nn_*.py mykeras5.ipynb tdrstyle.mplstyle
 scp hpe_default.tgz jlow@15.226.54.11:/home/uf/jlow/jftest2/slurm/
