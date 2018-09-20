239faef68a7847e9d9560b0a4ad03a0bd682edf9
Jia Fu Low <jia.fu.low@cern.ch>
2018-09-19 21:22:41 -0400

    Update mykeras6.ipynb [7]

--- Analyzers/test8/startup.sh
+++ Analyzers/test8/startup.sh
@@ -1,17 +1,15 @@
-#cd ~/L1MuonTrigger/PostProcessJobs/CMSSW_10_1_7/src/
-#cmsenv
-#source venv/bin/activate
-#cd -
-
-
 cd ~/L1MuonTrigger/PostProcessJobs/CMSSW_10_1_7/src/
 cmsenv
-#source venv/bin/activate
+source venv/bin/activate
 cd -
 
-# Anaconda
-export JFTEST=/home/jlow/L1MuonTrigger/PostProcessJobs/miniconda3
-export PATH=$JFTEST/bin:$PATH
-export LD_LIBRARY_PATH=$JFTEST/lib:$LD_LIBRARY_PATH
-source activate tensorflow_conda  # for tensorflow 1.10.0
+## Anaconda
+#cd ~/L1MuonTrigger/PostProcessJobs/CMSSW_10_1_7/src/
+#cmsenv
+##source venv/bin/activate
+#cd -
+#export JFTEST=/home/jlow/L1MuonTrigger/PostProcessJobs/miniconda3
+#export PATH=$JFTEST/bin:$PATH
+#export LD_LIBRARY_PATH=$JFTEST/lib:$LD_LIBRARY_PATH
+#source activate tensorflow_conda  # for tensorflow 1.10.0
 
