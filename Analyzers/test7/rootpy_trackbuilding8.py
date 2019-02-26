657bb1a984f1add0ec611c37c622254db18f4630
Jia Fu Low <jia.fu.low@cern.ch>
2019-02-25 19:25:28 -0500

    Update rootpy_trackbuilding8.py [14]

--- Analyzers/test7/rootpy_trackbuilding8.py
+++ Analyzers/test7/rootpy_trackbuilding8.py
@@ -360,6 +360,34 @@ class EMTFOldPhi(object):
   def __init__(self):
     self.ph_pattern_corr_lut = np.array([0, 0, 5, 5, 5, 5, 2, 2, 2, 2, 0], dtype=np.int32)
     self.ph_pattern_corr_sign_lut = np.array([0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0], dtype=np.int32)
+    self.ph_init_lut = np.array([
+      1324,1926,2527,1332,1932,2532,1415,2015,2615,1332,1932,2532,726,732,815,732,3128,3725,4325,3132,3732,4332,3214,3815,4415,3131,3732,4331,1316,2516,
+      3716,1332,1932,2532,3132,3732,4332,116,732,2580,3781,4980,1964,2564,3164,3764,4364,4964,1380,1364,2580,3780,4980,1964,2564,3164,3764,4364,4964,1380,
+      1364,1326,1923,2527,1332,1932,2532,1415,2015,2614,1331,1931,2531,725,732,815,731,3123,3724,4326,3132,3732,4332,3214,3814,4415,3131,3731,4331,1316,
+      2516,3715,1332,1932,2532,3132,3731,4331,116,732,2580,3780,4980,1964,2564,3164,3763,4363,4964,1380,1364,2580,3780,4979,1964,2564,3164,3763,4363,4963,
+      1380,1363,1323,1926,2525,1331,1932,2532,1415,2015,2615,1331,1931,2531,726,732,816,732,3124,3727,4325,3132,3732,4332,3215,3815,4415,3131,3731,4332,
+      1316,2516,3716,1332,1932,2532,3131,3731,4332,116,732,2580,3780,4980,1964,2564,3164,3764,4364,4964,1381,1364,2580,3780,4980,1964,2564,3164,3764,4364,
+      4964,1380,1364,1324,1929,2531,1332,1932,2532,1416,2015,2615,1331,1932,2531,725,732,815,732,3123,3728,4327,3132,3732,4332,3215,3815,4416,3132,3731,
+      4332,1316,2516,3716,1332,1932,2532,3132,3733,4332,116,732,2580,3781,4980,1964,2564,3165,3765,4365,4964,1380,1364,2580,3781,4981,1964,2564,3164,3765,
+      4365,4965,1380,1364,1325,1925,2524,1332,1932,2532,1415,2015,2615,1331,1931,2531,727,732,815,731,3124,3726,4325,3132,3732,4332,3215,3815,4415,3132,
+      3731,4331,1316,2516,3716,1332,1932,2532,3132,3732,4332,116,732,2580,3780,4980,1964,2564,3164,3764,4364,4964,1380,1364,2580,3780,4980,1964,2564,3164,
+      3764,4364,4964,1380,1364,1321,1927,2524,1332,1932,2532,1415,2015,2615,1331,1931,2532,725,732,815,731,3128,3727,4326,3133,3732,4332,3215,3815,4415,
+      3131,3731,4332,1316,2516,3716,1332,1932,2532,3132,3732,4332,116,732,2580,3780,4980,1964,2564,3164,3764,4364,4964,1380,1364,2580,3780,4980,1964,2564,
+      3164,3764,4364,4964,1380,1364,1979,2578,3178,1972,2572,3172,1890,2489,3090,1973,2573,3173,1380,1372,1289,1375,3779,4380,4978,3772,4372,4972,3689,4289,
+      4889,3772,4373,4972,2588,3788,4988,1972,2572,3172,3772,4372,4972,1388,1372,1324,2524,3724,1340,1940,2540,3140,3740,4340,124,740,1324,2524,3724,1340,
+      1940,2540,3140,3740,4340,124,740,1979,2578,3179,1972,2572,3172,1889,2489,3089,1973,2573,3173,1378,1372,1289,1372,3778,4380,4982,3772,4372,4972,3689,
+      4289,4890,3773,4373,4973,2588,3788,4988,1972,2572,3172,3772,4372,4972,1388,1372,1324,2524,3724,1340,1940,2540,3140,3740,4340,124,740,1324,2524,3724,
+      1340,1940,2540,3140,3740,4340,124,740,1977,2580,3179,1972,2572,3172,1889,2489,3089,1975,2572,3173,1382,1372,1289,1372,3779,4379,4979,3772,4372,4972,
+      3688,4289,4889,3773,4373,4973,2588,3788,4988,1972,2572,3172,3772,4372,4972,1388,1372,1324,2524,3724,1340,1940,2540,3140,3740,4340,124,740,1324,2524,
+      3724,1340,1940,2540,3140,3740,4340,124,740,1979,2577,3180,1972,2572,3172,1889,2489,3089,1973,2573,3173,1379,1372,1289,1373,3780,4378,4979,3772,4372,
+      4972,3689,4289,4889,3773,4373,4973,2588,3788,4988,1972,2572,3172,3772,4372,4972,1388,1372,1324,2524,3724,1340,1940,2540,3140,3740,4340,124,740,1324,
+      2524,3724,1340,1940,2541,3141,3741,4341,124,740,1978,2580,3179,1972,2572,3172,1889,2489,3089,1973,2573,3173,1380,1372,1290,1373,3780,4378,4981,3772,
+      4372,4972,3689,4290,4889,3775,4372,4977,2588,3787,4987,1972,2572,3172,3772,4372,4972,1388,1372,1324,2523,3723,1340,1940,2540,3140,3740,4340,124,740,
+      1324,2523,3724,1341,1941,2541,3141,3741,4341,124,741,1979,2581,3178,1973,2573,3173,1890,2490,3090,1973,2575,3173,1382,1373,1290,1377,3779,4380,4981,
+      3773,4373,4973,3690,4290,4890,3774,4374,4976,2589,3789,4989,1973,2573,3173,3773,4373,4973,1388,1373,1325,2525,3725,1341,1941,2541,3141,3741,4341,124,
+      741,1325,2525,3725,1341,1941,2541,3141,3741,4341,124,742
+    ], dtype=np.int32)
+    assert(len(self.ph_init_lut) == 2*6*61)
 
   def __call__(self, hit):
     emtf_phi = np.int32(hit.emtf_phi)
@@ -372,38 +400,84 @@ class EMTFOldPhi(object):
       # Is this 10-deg or 20-deg chamber?
       is_10degree = (hit.station == 1) or (hit.station >= 2 and hit.ring == 2)  # ME1 and ME2,3,4/2
 
-      # Undo CLCT comp digi fit phi correction
-      # (not totally correct!)
-      if is_10degree:
-        emtf_phi >>= 2
-        emtf_phi <<= 2
+      pc_station = -1
+      pc_chamber = -1
+      if hit.neighbor == 0:
+        if hit.station == 1:  # ME1: 0 - 8, 9 - 17
+          pc_station = hit.subsector-1
+          pc_chamber = hit.cscid-1
+        else:                 # ME2,3,4: 18 - 26, 27 - 35, 36 - 44
+          pc_station = hit.station
+          pc_chamber = hit.cscid-1
       else:
-        emtf_phi >>= 3
-        emtf_phi <<= 3
+        if hit.station == 1:  # ME1n: 45 - 47
+          pc_station = 5
+          pc_chamber = (hit.cscid-1)/3
+        else:                 # ME2n,3n,4n: 48 - 53
+          pc_station = 5
+          pc_chamber = (hit.station) * 2 - 1 + (0 if (hit.cscid-1 < 3) else 1)
+      assert(pc_station != -1)
+      assert(pc_chamber != -1)
 
-      # Re-apply CLCT pattern phi correction
-      # (not totally correct!)
-      clct_patt_corr = self.ph_pattern_corr_lut[clct_pattern]
-      clct_patt_corr_sign_ = self.ph_pattern_corr_sign_lut[clct_pattern]
-      clct_patt_corr_sign = 1 if clct_patt_corr_sign_ == 0 else -1
+      pc_lut_id = pc_chamber
+      if pc_station == 0:    # ME1 sub 1: 0 - 11
+        pc_lut_id = pc_lut_id + 9 if (hit.ring == 4) else pc_lut_id
+      elif pc_station == 1:  # ME1 sub 2: 16 - 27
+        pc_lut_id += 16
+        pc_lut_id = pc_lut_id + 9 if (hit.ring == 4) else pc_lut_id
+      elif pc_station == 2:  # ME2: 28 - 36
+        pc_lut_id += 28
+      elif pc_station == 3:  # ME3: 39 - 47
+        pc_lut_id += 39
+      elif pc_station == 4:  # ME4 : 50 - 58
+        pc_lut_id += 50
+      elif pc_station == 5 and pc_chamber < 3:  # neighbor ME1: 12 - 15
+        pc_lut_id = pc_lut_id + 15 if (hit.ring == 4) else pc_lut_id + 12
+      elif pc_station == 5 and pc_chamber < 5:  # neighbor ME2: 37 - 38
+        pc_lut_id += 28 + 9 - 3
+      elif pc_station == 5 and pc_chamber < 7:  # neighbor ME3: 48 - 49
+        pc_lut_id += 39 + 9 - 5
+      elif pc_station == 5 and pc_chamber < 9:  # neighbor ME4: 59 - 60
+        pc_lut_id += 50 + 9 - 7
+      assert(pc_lut_id < 61)
+
+      fw_sector = hit.sector-1
+      fw_endcap = 0 if (hit.endcap == 1) else 1
+      fw_strip = hit.strip  # already starts from 0
+
+      # Apply phi correction from CLCT pattern number
+      clct_pat_corr = self.ph_pattern_corr_lut[clct_pattern]
+      clct_pat_corr_sign_ = self.ph_pattern_corr_sign_lut[clct_pattern]
+      clct_pat_corr_sign = 1 if (clct_pat_corr_sign_ == 0) else -1
+      if fw_strip == 0 and clct_pat_corr_sign == -1:
+        clct_pat_corr = 0
+
       if is_10degree:
-        eighth_strip = clct_patt_corr_sign * (clct_patt_corr >> 1)
+        eighth_strip = fw_strip << 2  # full precision, uses only 2 bits of pattern correction
+        eighth_strip += clct_pat_corr_sign * (clct_pat_corr >> 1)
       else:
-        eighth_strip = clct_patt_corr_sign * (clct_patt_corr >> 0)
+        eighth_strip = fw_strip << 3  # multiply by 2, uses all 3 bits of pattern correction
+        eighth_strip += clct_pat_corr_sign * (clct_pat_corr >> 0)
+      assert(eighth_strip >= 0)
 
       # Multiplicative factor for eighth_strip
-      mult_factor = 1024
+      factor = 1024
       if hit.station == 1 and hit.ring == 4:
-        mult_factor = 1707
+        factor = 1707
       elif hit.station == 1 and hit.ring == 1:
-        mult_factor = 1301
+        factor = 1301
       elif hit.station == 1 and hit.ring == 3:
-        mult_factor = 947
+        factor = 947
 
-      ph_tmp = (eighth_strip * mult_factor) >> 10
-      ph_tmp_sign = 1 if ph_reverse == 0 else -1
-      emtf_phi += ph_tmp_sign * ph_tmp
-    else:
+      ph_tmp = (eighth_strip * factor) >> 10
+      ph_tmp_sign = 1 if (ph_reverse == 0) else -1
+
+      endsec_pc_lut_id = (fw_endcap * 6 + fw_sector) * 61 + pc_lut_id
+      fph = self.ph_init_lut[endsec_pc_lut_id] + ph_tmp_sign * ph_tmp
+      assert(0 <= fph and fph < 5000)
+      #
+      emtf_phi = fph
+    else:  # non-CSC
       pass
     return emtf_phi
 
@@ -412,7 +486,9 @@ class EMTFTheta(object):
   def __call__(self, hit):
     emtf_theta = np.int32(hit.emtf_theta)
     if hit.type == kDT:
-      if hit.wire == -1:
+      # wire -1 means no theta SL
+      # quality 0&1 are RPC digis
+      if (hit.wire == -1) or (hit.quality < 2):
         if hit.station == 1:
           emtf_theta = 112
         elif hit.station == 2:
@@ -567,7 +643,7 @@ def weighted_percentile(data, percents, weights=None):
 # ______________________________________________________________________________
 # Data Formats
 
-PARTICLE_NVARS = 5
+PARTICLE_NVARS = 6
 
 class Particle(object):
   def __init__(self, pt, eta, phi, q, vx, vy, vz):
@@ -581,8 +657,8 @@ class Particle(object):
 
   def to_parameters(self):
     # Convert into an entry in a numpy array
-    # (q/pT, phi, eta, dxy, dz)
-    parameters = np.array((np.true_divide(self.q, self.pt), self.phi, self.eta, root_sum_square(self.vx, self.vy), self.vz), dtype=np.float32)
+    # (q/pT, phi, eta, vx, vy, vz)
+    parameters = np.array((np.true_divide(self.q, self.pt), self.phi, self.eta, self.vx, self.vy, self.vz), dtype=np.float32)
     return parameters
 
 class PatternBank(object):
