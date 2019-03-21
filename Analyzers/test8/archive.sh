4ae91c08fa3a3edc6a77319d74b70aaad26effc0
Jia Fu Low <jia.fu.low@cern.ch>
2019-03-21 00:33:37 -0400

    Clean up test8

---
+++ Analyzers/test8/archive.sh
@@ -0,0 +1,2 @@
+#tar czf default_nb.tgz nn_*.py mykeras6.ipynb tdrstyle.mplstyle model.h5 model.json model_weights.h5
+tar czf default_nb.tgz nn_*.py mykeras10.ipynb mykeras10_omtf.ipynb tdrstyle.mplstyle model.h5 model.json model_weights.h5 model_omtf.h5 model_omtf.json model_omtf_weights.h5
