1326a703f60359d2c40303d002dd111b95b69c0d
Jia Fu Low <jia.fu.low@cern.ch>
2019-06-03 11:11:00 -0400

    Add convert_to_constant_graph.py
    Also add model_graph.26.pb

---
+++ Analyzers/test7/convert_to_constant_graph.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import sys
+
+def usage():
+  print('usage: python {0} FILE'.format(sys.argv[0]))
+  print('')
+  print('arguments:')
+  print('  FILE    a model JSON file, e.g. \'model.json\'')
+
+if __name__ == "__main__":
+  if len(sys.argv) < 2:
+    usage()
+    sys.exit(1)
+
+  model_file = sys.argv[1]
+  model_weights_file = model_file.replace('model', 'model_weights').replace('.json', '.h5')
+  constant_graph_file = model_file.replace('model', 'model_graph').replace('.json', '.pb')
+
+  from nn_models import load_my_model, update_keras_custom_objects
+  update_keras_custom_objects()
+
+  loaded_model = load_my_model(name=model_file, weights_name=model_weights_file)
+
+  import tensorflow as tf
+  from keras import backend as K
+  sess = K.get_session()
+
+  # save as a constant graph
+  _input = loaded_model.input.op.name
+  outputs = [node.op.name for node in loaded_model.outputs]
+  print('input: {}'.format(_input))
+  print('outputs: {}'.format(outputs))  # names of output operations you want to use later
+  constant_graph = tf.graph_util.convert_variables_to_constants(
+      sess, sess.graph.as_graph_def(), outputs)
+  tf.train.write_graph(constant_graph, './', constant_graph_file, as_text=False)
+  print('constant graph: {}'.format(constant_graph_file))
